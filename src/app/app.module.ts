669e7bec8a4fd59e7f06c4fccd31f2c005b25ef6
Tommy Heyding <toh@hbt.de>
2021-12-31 00:46:12 +0100

    Add ngx-translate and translation file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { IntroComponent } from './components/intro/intro.component';
-import { NotesComponent } from './components/notes/notes.component';
-import { AnnouncementComponent } from './components/announcement/announcement.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {AppComponent} from './app.component';
+import {IntroComponent} from './components/intro/intro.component';
+import {NotesComponent} from './components/notes/notes.component';
+import {AnnouncementComponent} from './components/announcement/announcement.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,24 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // ngx-translate and the loader module
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
