321d61265767744b5d02518816ab868e1533df86
Tommy Heyding <toh@hbt.de>
2021-12-31 00:15:57 +0100

    Add basic tailwind components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NotesComponent,
+    AnnouncementComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
