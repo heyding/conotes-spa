48373e1b43a365d690240d3473ee302805ac5764
Tommy Heyding <toh@hbt.de>
2022-01-02 16:17:04 +0100

    Add announcement component

--- src/app/components/announcement/announcement.component.ts
+++ src/app/components/announcement/announcement.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-announcement',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnouncementComponent implements OnInit {
 
-  constructor() { }
+  dismiss: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  dismissAnnouncement(): void {
+    this.dismiss = true;
+  }
+
 
 }
