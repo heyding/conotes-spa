b2970075bc326d0d8e3ef390f223cc1e3e1f1edb
Tommy Heyding <toh@hbt.de>
2022-01-02 15:02:58 +0100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   prefix: '',
-  purge: {
-    enabled: true,
-    content: [
-      './src/**/*.{html,ts}',
-    ]
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
